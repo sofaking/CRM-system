46362abb610e404b68b3700c8c8d92486d7d53eb
Vladislav Petrov <electronicchest@gmail.com>
2013-09-03 15:23:43 +0400

    Models joins - user to account to projects

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,4 @@
 class Account < ActiveRecord::Base
+  has_many :projects, dependent: :delete_all
+  belongs_to :user, touch: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
+  belongs_to :account
   has_many :todo_lists, dependent: :delete_all
 end
