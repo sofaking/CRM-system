8390412b91f60b483d4a6a20b22e4c042be580b5
Vladislav Petrov <electronicchest@gmail.com>
2013-09-04 20:30:11 +0400

    Sessions#create - redirection to user account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to home_url
+      redirect_to user.account
     else
       redirect_to login_url, alert: "Invalid user/password combination"
     end
