bf62028c2c02cb6f4abafb4427f350ba51576d0b
Mike Zagorulko <miralas777@yandex.ru>
2013-09-04 15:09:20 +0400

    Event model add function as json

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,14 @@
 class Event < ActiveRecord::Base
+  def as_json(options = {})
+      {
+        id: self.id,
+        title: self.title,
+        description: self.description || "",
+        start: starts_at.rfc822,
+        end: ends_at.rfc822,
+        allDay: self.all_day,
+        recurring: false,
+        url: Rails.application.routes.url_helpers.event_path(id)
+      }
+    end
 end
