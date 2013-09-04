bc7a317bc314edd1cff7e0155fc4c24de1dfe548
Mike Zagorulko <miralas777@yandex.ru>
2013-09-04 15:21:43 +0400

    Fix bug with rendering js calendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -2,7 +2,7 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(document).ready(function() {
+$(document).on('ready page:load', function() {
 
     // page is now ready, initialize the calendar...
 
