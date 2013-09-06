39407f64c3310c20a0de8dc6e8e2fbb212927173
Mike Zagorulko <miralas777@yandex.ru>
2013-09-06 20:51:20 +0400

    Controller#home - change index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :authorize
   
   def index
-      @projects = Project.first
+    redirect_to current_user.account if current_user
   end
 end
