8e64e5f8b58e4a73c76269b15470d6b3dbef7c2b
Mike Zagorulko <miralas777@yandex.ru>
2013-09-09 18:48:33 +0400

    Fix bugs after merging branches Master and Discussion

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,11 @@ class HomeController < ApplicationController
   skip_before_action :authorize
   
   def index
-<<<<<<< HEAD
-    redirect_to current_user.account if current_user
-=======
     if current_user
       redirect_to current_user.account
     else
       @user = User.new
       render layout: false
     end
->>>>>>> master
   end
 end
