4f402a8ad5f4dd0a614d9a6a5699f6317f6bf8cd
Vladislav Petrov <electronicchest@gmail.com>
2013-09-11 01:32:10 +0400

    Events - add normilize from all day

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,26 +1,30 @@
 module EventsHelper
-  def normilize_event_date(start, ends)
-    if start.utc == ends.utc
-      "#{shortening_date start}"
+  def normilize_event_date(start, ends, all_day)
+    if start.utc == ends.utc or all_day
+      "#{shortening_date start, all_day}"
     else
-      "between #{shortening_date start} and #{shortening_date ends}"
+      "between #{shortening_date start, all_day} and #{shortening_date ends, all_day}"
     end
   end
   
   private
     
-    def shortening_date(date)
+    def shortening_date(date, all_day)
       format = ""
       format += "%e"  unless date.strftime("%e %a %B") == Date.today.strftime("%e %a %B")
-      format += " %a" unless date.strftime("%a %B") == Date.today.strftime("%a %B")
+      format += " %A" unless date.strftime("%a %B") == Date.today.strftime("%a %B")
       format += " %B" unless date.strftime("%B") == Date.today.strftime("%B")
       format += " %Y" unless date.strftime("%Y") == Date.today.strftime("%Y")
-      format += " today at"  if date.strftime("%e %a %B %Y") == Date.today.strftime("%e %a %B %Y")
-      format += " %k"
-      unless date.strftime("%M").to_i == 0
-        format += ":%M"
+      if all_day
+        format += " all day"
       else
-        format += " hours"
+        format += " today at"  if date.strftime("%e %a %B %Y") == Date.today.strftime("%e %a %B %Y")
+        format += " %k"
+        unless date.strftime("%M").to_i == 0
+          format += ":%M"
+        else
+          format += " hours"
+        end
       end
       date.strftime(format)
     end
