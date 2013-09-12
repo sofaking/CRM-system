9ff452e134cd59fbc7fd030f1cc8ca7644773f63
Vladislav Petrov <electronicchest@gmail.com>
2013-09-12 20:32:07 +0400

    Events model - fix some bugs

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,15 +3,17 @@ class Event < ActiveRecord::Base
   belongs_to :project
   
   def as_json(options = {})
-      {
-        id: self.id,
-        title: self.title + ' - ' + self.project.name,
-        description: self.description || "",
-        start: starts_at.rfc822,
-        end: ends_at.rfc822,
-        allDay: self.all_day,
-        recurring: false,
-        url: Rails.application.routes.url_helpers.edit_account_event_path(self.project.account.id, id)
-      }
-    end
+    calendar_name = ' - ' + (self.project ? self.project.name : 'General')
+
+    {
+      id: self.id,
+      title: self.title + calendar_name,
+      description: self.description || "",
+      start: starts_at.rfc822,
+      end: ends_at.rfc822,
+      allDay: self.all_day,
+      recurring: false,
+      url: Rails.application.routes.url_helpers.edit_account_event_path(self.account.id, id)
+    }
+  end
 end
