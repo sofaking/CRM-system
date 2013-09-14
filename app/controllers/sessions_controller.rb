ea3b010e0af164dca706b00e4e92330734aa4d13
Vladislav Petrov <electronicchest@gmail.com>
2013-09-15 01:42:06 +0400

    Sessions#create - change redirection

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user.account
     else
-      redirect_to login_url, alert: "Invalid user/password combination"
+      redirect_to home_url, notice: "Invalid user/password combination"
     end
   end
 
