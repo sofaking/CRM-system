f8fbf02404f27b5f81b8130132b77d4e7e3ff253
Vladislav Petrov <electronicchest@gmail.com>
2013-09-15 01:42:31 +0400

    Application# - add workspaces

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
+  #before_action :authorize
 
   #before_action :authorize
   
@@ -8,10 +9,55 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   protected
-  
+    
     def authorize
-      if not User.find_by(id: session[:user_id])
-        redirect_to login_url, notice: "Please log in"
+      if current_user
+        if current_user.client?
+          redirect_to home_url unless client_workplace
+        elsif current_user.visiter?
+          redirect_to home_url unless visiter_workplace
+        elsif current_user.admin?
+          redirect_to home_url unless admin_workplace
+        else
+          redirect_to home_url
+        end
+      else
+        redirect_to login_url, notice: "Please log in" unless logout_workplace
+      end
+    end
+    
+    def logout_workplace
+      request_to controller: ['home'] or
+      request_to controller: ['sessions', 'users'], action: ['new', 'create']
+    end
+    
+    def client_workplace
+      request_to controller: ['sessions', 'home', 'todolists', 'todos', 'events'] or
+      request_to controller: ['accounts'], action: ['show'] or
+      request_to controller: ['projects'], action: ['new', 'create', 'destroy', 'show', 'edit', 'update']
+      
+    end
+      
+    def visiter_workplace
+      request_to controller: ['sessions', 'home'] or
+      request_to controller: ['accounts'], action: ['show'] or
+      request_to controller: ['projects'], action: ['show'] or
+      request_to controller: ['todolists', 'todos', 'events'], action: ['index', 'show']
+    end
+    
+    def admin_workplace
+      true # all places
+    end
+    
+    def request_to args={}
+      if args[:controller].include? params[:controller]
+        if args[:action]
+          args[:action].include? params[:action]
+        else
+          true
+        end
+      else
+        false
       end
     end
 end
