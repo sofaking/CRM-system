6ecbfc6d6b17457008fb131fdfcaa1d35afd7e7c
Vladislav Petrov <electronicchest@gmail.com>
2013-09-15 01:40:05 +0400

    Join roles to users and create role functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,28 @@
 class User < ActiveRecord::Base
+  belongs_to :role
   has_one :account, dependent: :delete
   has_secure_password
   
   before_validation on: :create do
     self.email = email.downcase if attribute_present?("email")
-    self.account = Account.create
+    self.role ||= Role.find_by(title: "client")
+    self.account ||= Account.create
   end
   
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, on: :create
-  validates_presence_of :email, :account
+  validates_presence_of :email
+  validates_associated :account
   validates_uniqueness_of :email
+  
+  def admin?
+    role.title == 'admin'
+  end
+  
+  def visiter?
+    role.title == 'visiter'
+  end
+  
+  def client?
+    role.title == 'client'
+  end
 end
