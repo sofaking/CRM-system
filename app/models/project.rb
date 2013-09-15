2416fcad56411d91d2a048cb1d0ccd9ef208e91b
Vladislav Petrov <electronicchest@gmail.com>
2013-09-15 04:18:50 +0400

    Fix name bug in Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  has_and_belongs_to_many :account
+  has_and_belongs_to_many :accounts
   has_many :todo_lists, dependent: :delete_all
   has_many :events, dependent: :delete_all
 end
