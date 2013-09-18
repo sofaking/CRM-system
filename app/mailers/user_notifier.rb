f72a98ad0b3e2de606d1bf9b954ae91564123475
Vladislav Petrov <electronicchest@gmail.com>
2013-09-19 01:26:33 +0400

    Generate mailer UserNotifier
    rails generate mailer UserNotifier welcome added_to_project

---
+++ app/mailers/user_notifier.rb
@@ -0,0 +1,25 @@
+class UserNotifier < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_notifier.welcome.subject
+  #
+  def welcome
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_notifier.added_to_project.subject
+  #
+  def added_to_project
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
